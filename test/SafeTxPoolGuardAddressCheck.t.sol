10b3f2f3c54dc259b0931215bd10c108e3cb76d0
Ha DANG <dvietha@gmail.com>
2025-04-15 10:16:20 +0700

    Fix formatting in SafeTxPoolGuardAddressCheck.t.sol

--- test/SafeTxPoolGuardAddressCheck.t.sol
+++ test/SafeTxPoolGuardAddressCheck.t.sol
@@ -7,17 +7,12 @@ import "@safe-global/safe-contracts/contracts/common/Enum.sol";
 
 contract MockSafeWithGuard {
     SafeTxPool public guard;
-    
+
     constructor(SafeTxPool _guard) {
         guard = _guard;
     }
-    
-    function executeTransaction(
-        address to,
-        uint256 value,
-        bytes calldata data,
-        Enum.Operation operation
-    ) external {
+
+    function executeTransaction(address to, uint256 value, bytes calldata data, Enum.Operation operation) external {
         // Call checkTransaction on the guard
         guard.checkTransaction(
             to,
@@ -32,7 +27,7 @@ contract MockSafeWithGuard {
             bytes(""), // signatures
             msg.sender // msgSender
         );
-        
+
         // If the guard doesn't revert, the transaction would execute
     }
 }
@@ -44,107 +39,77 @@ contract SafeTxPoolGuardAddressCheckTest is Test {
     address public addressInBook;
     address public addressNotInBook;
     bytes32 public nameInBook;
-    
+
     function setUp() public {
         // Setup test accounts
         owner = address(0x1234);
         addressInBook = address(0xABCD);
         addressNotInBook = address(0x5678);
         nameInBook = bytes32("AllowedAddress");
-        
+
         // Deploy SafeTxPool
         pool = new SafeTxPool();
-        
+
         // Deploy mock Safe with the guard
         mockSafe = new MockSafeWithGuard(pool);
-        
+
         // Add an address to the address book (prank as the mock Safe since only Safe can manage its address book)
         vm.prank(address(mockSafe));
         pool.addAddressBookEntry(address(mockSafe), addressInBook, nameInBook);
     }
-    
+
     function testAllowTransactionToAddressInAddressBook() public {
         // This should succeed since addressInBook is in the address book
-        mockSafe.executeTransaction(
-            addressInBook,
-            1 ether,
-            hex"",
-            Enum.Operation.Call
-        );
-        
+        mockSafe.executeTransaction(addressInBook, 1 ether, hex"", Enum.Operation.Call);
+
         // If we got here without reverting, the test passes
         assertTrue(true);
     }
-    
+
     function testRevertTransactionToAddressNotInAddressBook() public {
         // This should revert since addressNotInBook is not in the address book
         vm.expectRevert(SafeTxPool.AddressNotInAddressBook.selector);
-        mockSafe.executeTransaction(
-            addressNotInBook,
-            1 ether,
-            hex"",
-            Enum.Operation.Call
-        );
+        mockSafe.executeTransaction(addressNotInBook, 1 ether, hex"", Enum.Operation.Call);
     }
-    
+
     function testAddAddressAndThenAllowTransaction() public {
         // First attempt should revert
         vm.expectRevert(SafeTxPool.AddressNotInAddressBook.selector);
-        mockSafe.executeTransaction(
-            addressNotInBook,
-            1 ether,
-            hex"",
-            Enum.Operation.Call
-        );
-        
+        mockSafe.executeTransaction(addressNotInBook, 1 ether, hex"", Enum.Operation.Call);
+
         // Add the address to the address book - prank as the mockSafe
         vm.prank(address(mockSafe));
         pool.addAddressBookEntry(address(mockSafe), addressNotInBook, bytes32("NewlyAddedAddress"));
-        
+
         // Now the transaction should succeed
-        mockSafe.executeTransaction(
-            addressNotInBook,
-            1 ether,
-            hex"",
-            Enum.Operation.Call
-        );
-        
+        mockSafe.executeTransaction(addressNotInBook, 1 ether, hex"", Enum.Operation.Call);
+
         // If we got here without reverting, the test passes
         assertTrue(true);
     }
-    
+
     function testRemoveAddressAndThenRevertTransaction() public {
         // First transaction should succeed
-        mockSafe.executeTransaction(
-            addressInBook,
-            1 ether,
-            hex"",
-            Enum.Operation.Call
-        );
-        
+        mockSafe.executeTransaction(addressInBook, 1 ether, hex"", Enum.Operation.Call);
+
         // Remove the address from the address book - prank as the mockSafe
         vm.prank(address(mockSafe));
         pool.removeAddressBookEntry(address(mockSafe), addressInBook);
-        
+
         // Now the transaction should revert
         vm.expectRevert(SafeTxPool.AddressNotInAddressBook.selector);
-        mockSafe.executeTransaction(
-            addressInBook,
-            1 ether,
-            hex"",
-            Enum.Operation.Call
-        );
+        mockSafe.executeTransaction(addressInBook, 1 ether, hex"", Enum.Operation.Call);
     }
-    
+
     function testRevertWhenNonSafeAddsToAddressBook() public {
         // Try to add an address to the address book as a non-Safe wallet
         vm.expectRevert(SafeTxPool.NotSafeWallet.selector);
         pool.addAddressBookEntry(address(mockSafe), addressNotInBook, bytes32("Unauthorized"));
     }
-    
+
     function testRevertWhenNonSafeRemovesFromAddressBook() public {
         // Try to remove an address from the address book as a non-Safe wallet
         vm.expectRevert(SafeTxPool.NotSafeWallet.selector);
         pool.removeAddressBookEntry(address(mockSafe), addressInBook);
     }
-} 
+}
